b290dfb465a3de33a56aeffa30dcf9e23bf9005f
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-24 08:29:45 +0300

    сделал первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] Numbers = new int[M];
+
+void InputArray(int M)
+{
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    Numbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int AboveZero(int[] Numbers)
+{
+  int count = 0;
+  for (int i = 0; i < Numbers.Length; i++)
+  {
+    if(Numbers[i] > 0 ) 
+    {
+        count += 1; 
+    }
+  }
+  return count;
+}
+
+InputArray(M);
+
+int result = AboveZero(Numbers);
+
+Console.WriteLine($"Введено чисел больше 0: {result} ");
